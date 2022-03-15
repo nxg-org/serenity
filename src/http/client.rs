75c2960871e753fc364197bc3e4fb5990038c745
GenerelSchwerz <xXSG.KingXx@gmail.com>
2022-03-15 13:47:42 -0400

    fuck

--- src/http/client.rs
+++ src/http/client.rs
@@ -3687,7 +3687,7 @@ impl Http {
         let response = if self.ratelimiter_disabled {
             let request = req.build(&self.client, &self.token, self.proxy.as_ref())?.build()?;
 
-            println!("{:?}", &request.headers);
+            println!("{:?}", &request.headers());
             self.client.execute(request).await?
         } else {
             let ratelimiting_req = RatelimitedRequest::from(req);
